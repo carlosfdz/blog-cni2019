4be5cc82b6dedc550216af04e831cace38a1ad9d
Carlos Fernandez <fernandez.isc@gmail.com>
2019-10-24 13:48:21 -0500

    Protegiendo rutas.

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,14 @@
 //     return App\Post::find($id);
 // });
 
-Route::resource('posts', 'PostController');
+
 
 Route::get('/', 'HomeController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
-	
+	Route::resource('posts', 'PostController');
 });
 
 Auth::routes();
